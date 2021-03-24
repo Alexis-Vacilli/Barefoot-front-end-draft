9ec1bc75f3d86c35bc0086398c8d3317233809f3
Alexis-Vacilli <alexisvacilli1@gmail.com>
2021-03-24 19:39:16 +0200

    ft-create-role

--- src/components/rolesAndPermissions/rolesAndPermissions.js
+++ src/components/rolesAndPermissions/rolesAndPermissions.js
@@ -7,6 +7,7 @@ import PropTypes from "prop-types";
 
 class RolesAndPermissions extends Component {
     state = { 
+        newRole: '',
         roles: [],
         permissions: [],
      }
@@ -14,16 +15,32 @@ class RolesAndPermissions extends Component {
         roles: PropTypes.func.isRequired,
         getRoles: PropTypes.array.isRequired,
       };
+      onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+      };
+      onSumbit = (e) => {
+        e.preventDefault();
+        const {newRole} = this.state;
+      }
      componentDidMount(){
+        // alert(this.props.getRoles); 
         //  const roles = [...this.props.getRoles];
         //  this.setState({roles})
-        this.props.roles();
+        const data = this.props.roles();
+        console.log(data);
      }
+     componentDidUpdate(prevProps, prevState) {
+      console.log(prevProps);
+      if (prevProps.getRoles !== this.state.roles) {
+        this.setState({roles: prevProps.getRoles})
+      }
+    }
     render() { 
-        const {roles} = this.state;
-        alert('-----------',this.props.getRoles);
+        // const {roles} = this.state;
         return ( 
-            <div>
+            <div> 
+              <input name = "role" type="text" placeholder="role" onChange= {this.onChange} />
+              <button onClick>Add</button>
                 <table>
   <thead>
     <tr>
@@ -33,8 +50,8 @@ class RolesAndPermissions extends Component {
   </thead>
   <tbody>
     <tr>
-      {roles.map((role) => (
-          <td>{role.name}</td>
+      {this.props.getRoles.map((role) => (
+          <td key={role.id}>{role.name}</td>
       ))}
       <td></td>
     </tr>
